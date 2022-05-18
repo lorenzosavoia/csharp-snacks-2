4cc15a6b9fa1a3f6fb4d7583dce863d3001891ba
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-05-18 17:02:00 +0200

    jj

--- Program.cs
+++ Program.cs
@@ -110,6 +110,225 @@ namespace csharp_snacks_2
             //tutti gli elementi di una stringa da numero intero a numero intero => nuovo = f(vecchio);
             //Purtroppo per come è stata costruita, questa funzione si applica esclusivamente alle lista di numeri interi 
             //e torna una lista di numeri interi
+
+            List<string> numbersStrLst = new List<string>
+            { "One", "Two", "Three","Four","Five"};
+            // metodo join per stampare le liste o array
+            Console.WriteLine(String.Join(", ", numbersStrLst));//Output:"One, Two, Three, Four, Five"
+            // metodo join per stampare le liste o array
+            int[] numbersIntAry = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(String.Join("; ", numbersIntAry));//Output:"1; 2; 3; 4; 5"
+
+            Random random = new Random();
+            List<int> lsInt = new List<int>();
+            for(int i = 0; i < 1000; i++)
+            {
+                int iPush = random.Next(0, 999);
+                lsInt.Add(iPush);
+            }
+            //sortedList quando pusho dentro un elemento me lo mette giá in ordine
+            SortedSet<int> lsOrdinati = new SortedSet<int>();
+            foreach(int n in lsInt)
+            {
+                lsOrdinati.Add(n);
+            }
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(String.Join(", ", lsInt));
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(String.Join(", ", lsOrdinati));
+
+            bool[] vBool = new bool[1000];
+            foreach (var n in lsInt)
+            {
+                if ((n % 1) == 0)
+                {
+                    vBool[n] = true;
+                    Console.WriteLine(n);
+                    Console.WriteLine(vBool[n]);
+                }
+                else
+                {
+                    vBool[n] = false;
+                    Console.WriteLine(n);
+                    Console.WriteLine(vBool[n]);
+                }
+            }
+            Console.WriteLine(String.Join("\n", vBool));
+
+            var d = 3.4;
+            Console.WriteLine((d % 1) == 0);
+            var x = 0;
+            Console.WriteLine((x % 1) == 0);
+
+
+
+
+            /////////////////////////////////////////////////////////////////////////////////////////
+            
+            Random rng = new Random();
+
+            List<int> li_base = new List<int>();
+
+            var start = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            for (int i = 0; i < 1000; i++)
+            {
+                li_base.Add(rng.Next(0, 1000));
+            }
+            var end = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            //Console.WriteLine(end - start);
+
+            //Costruzione della lista
+            start = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            List<int> li = new List<int>();
+            foreach (int n in li_base)
+            {
+                li.Add(n);
+            }
+            end = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            Console.WriteLine("Tempo di insert in una lista: {0}", end - start);
+
+            //-calcolare quanto tempo impiegate per eseguire il seguente codice
+            //     - per 10000 volte
+            //        - prende in sequenza i numeri presenti in lista base
+            //          - verifica se n è presente nella lista li (quindi li.find...). Non dovete stampare nulla
+
+
+            int trovati = 0;
+            int nontrovati = 0;
+            start = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            for (int i = 0; i < 10000; ++i)
+            {
+                foreach (int n in li_base)
+                {
+                    //n è presente nella lista li?
+                    if (li.Contains(n))
+                        trovati++;
+                    else
+                        nontrovati++;
+                }
+            }
+            end = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            Console.WriteLine("Tempo di 10000 ricerche di 1000 elementi in una lista: {0}", end - start);
+            Console.WriteLine("Trovati: {0}", trovati);
+
+
+            //Ora voglio utilizzare un vettore
+            //Costruzione del vettore
+            start = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            int[] vi = new int[1000];
+
+            int posv = 0;
+            foreach (int n in li_base)
+            {
+                vi[posv++] = n;
+            }
+            end = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            Console.WriteLine("Tempo di insert in un vettore: {0}", end - start);
+
+
+            trovati = 0;
+            nontrovati = 0;
+            start = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            for (int i = 0; i < 10000; ++i)
+            {
+                foreach (int n in li_base)
+                {
+                    //n è presente nel vettore vi?
+                    //vi[3, 7, 1, 9, 8, 7, 6, 5, 4, 120, ....]
+                    //for (int pos=0; pos < 1000; ++pos)
+                    //    if (vi[pos] == n)
+                    //    {
+                    //        //l'ho trovato
+                    //        trovati++;
+                    //        break;
+                    //    }
+                    if (vi.Contains(n))
+                        trovati++;
+                    else
+                        nontrovati++;
+                }
+            }
+            end = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            Console.WriteLine("Tempo di 10000 ricerche di 1000 elementi in un vettore: {0}", end - start);
+            Console.WriteLine("Trovati: {0}", trovati);
+
+
+
+
+            //Ora voglio utilizzare un vettore
+            //Costruzione del vettore
+            start = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            bool[] vb = new bool[1000];  //sta tutto a false
+
+
+            //li_base = new List<int> { 6, 12, 99, 101, 456 };
+            foreach (int n in li_base)
+            {
+                vb[n] = true;
+            }
+            end = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            Console.WriteLine("Tempo di insert in un vettore: {0}", end - start);
+
+
+            trovati = 0;
+            nontrovati = 0;
+            start = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            for (int i = 0; i < 10000; ++i)
+            {
+                foreach (int n in li_base)
+                {
+                    if (vb[n])
+                        trovati++;
+                    else
+                        nontrovati++;
+                }
+            }
+            end = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            Console.WriteLine("Tempo di 10000 ricerche di 1000 elementi in un vettore booleno: {0}", end - start);
+            Console.WriteLine("Trovati: {0}", trovati);
+
+
+
+            //Ora voglio utilizzare un sorted set
+            //Costruzione del set
+            start = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            SortedSet<int> ssi = new SortedSet<int>();
+
+
+            foreach (int n in li_base)
+            {
+                ssi.Add(n);
+            }
+            end = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            Console.WriteLine("Tempo di insert in un ss: {0}", end - start);
+
+
+            trovati = 0;
+            nontrovati = 0;
+            start = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            for (int i = 0; i < 10000; ++i)
+            {
+                foreach (int n in li_base)
+                {
+                    if (ssi.Contains(n))
+                        trovati++;
+                    else
+                        nontrovati++;
+                }
+            }
+            end = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            Console.WriteLine("Tempo di 10000 ricerche di 1000 elementi in un sorted set: {0}", end - start);
+            Console.WriteLine("Trovati: {0}", trovati);
+
+            //!!!LA ricerca in un insieme ordinato è logaritmica
+            ///Se l'insieme contien N elementi, il tempo di ricerca oppure
+            ///il numero di operazioni eseguite per la ricerca è pari a O(log(N));
+            ///
+            ///La ricerca in un insieme sequenziale (non ordinato) è lineare
+            ///Se l'insieme contiene N elementi, il tempo di ricerca oppure
+            ///il numero di operazioni eseguite per la ricerca è pari a O(N);
+
         }
     }
 }
